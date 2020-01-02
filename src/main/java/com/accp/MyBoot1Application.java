68d498f154e5bb27afcfd4db04c2e64d04a5c8ee
mj <2657340193@qq.com>
2020-01-02 10:04:44 +0800

    第一次提交代码库

---
+++ src/main/java/com/accp/MyBoot1Application.java
@@ -0,0 +1,13 @@
+package com.accp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBoot1Application.class, args);
+	}
+
+}
